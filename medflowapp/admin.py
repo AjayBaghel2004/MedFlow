46de81fd6db6c9968bb177939adbbce93da9753e
Ajay Baghel <ajaybaghel2459@gmail.com>
2026-04-14 12:39:43 +0530

    Just added new models in models.py

--- medflowapp/admin.py
+++ medflowapp/admin.py
@@ -1,11 +1,29 @@
 from django.contrib import admin
-from .models import Employee, Medicines
+from .models import Employee, Medicines, Customers,  Suppliers, Purchases, Sales, SalesItems
 # Register your models here.
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ('id','name', 'email', 'role', 'employee_ID')
 admin.site.register(Employee, EmployeeAdmin)
 
-# admin.site.register(Customers)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display=('id', 'customer_name', 'customer_phone', 'customer_email', 'customer_address', 'created_at')
+admin.site.register(Customers, CustomerAdmin)
 class MedicineAdmin(admin.ModelAdmin):
     list_display=('id','medicine_name', 'med_category', 'expiry_date', 'stock_status','price', 'quantity')
 admin.site.register(Medicines, MedicineAdmin)
+
+class SuppliersAdmin(admin.ModelAdmin):
+    list_display =('id', 'supplier_name', 'supplier_contact', 'supplier_person', 'supplier_address')
+admin.site.register(Suppliers, SuppliersAdmin)
+
+class PurchasesAdmin(admin.ModelAdmin):
+    list_display=('id', 'supplier_ID','medicine_ID', 'quantity_received', 'cost_price', 'purchase_date')
+admin.site.register(Purchases, PurchasesAdmin)
+
+class SalesAdmin(admin.ModelAdmin):
+    list_display=('id', 'invoice_number', 'employee_id', 'subtotal', 'gst_amount', 'total_amount', 'sale_date')
+admin.site.register(Sales, SalesAdmin)
+
+class SalesItemsAdmin(admin.ModelAdmin):
+    list_display = ('id','medicine_ID', 'sale_ID', 'quantity_sold', 'unit_price', 'total_price')
+admin.site.register(SalesItems, SalesItemsAdmin)
--- medflowapp/models.py
+++ medflowapp/models.py
@@ -1,10 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     email=models.EmailField(max_length=50)
-    # ADMIN='Admin'
-    # STAFF='Staff'
     roles_choices={
         "Administrator":"Administrator",
         "Staff":"Staff"
@@ -12,6 +11,14 @@ class Employee(models.Model):
     role=models.CharField(choices=roles_choices)
     employee_ID=models.PositiveIntegerField()
     password=models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=timezone.now())
+
+class Customers(models.Model):
+    customer_name = models.CharField(max_length=100)
+    customer_phone = models.IntegerField()
+    customer_email = models.EmailField(max_length=100)
+    customer_address = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now())
 
 class Medicines(models.Model):
     medicine_name = models.CharField(max_length=50)
@@ -29,5 +36,33 @@ class Medicines(models.Model):
     }
     med_category = models.CharField(choices=category, default="----")
     stock_status = models.CharField(choices=status, default='Overstock')
-    price=models.FloatField()
+    price=models.DecimalField(max_digits=11, decimal_places=2)
     quantity = models.IntegerField()
+
+class Suppliers(models.Model):
+    supplier_name=models.CharField(max_length=50)
+    supplier_contact = models.IntegerField()
+    supplier_person = models.CharField()
+    supplier_address=models.TextField()
+
+class Purchases(models.Model):
+    supplier_ID = models.ForeignKey(Suppliers, on_delete=models.CASCADE)
+    medicine_ID = models.ForeignKey(Medicines, on_delete=models.CASCADE)
+    quantity_received = models.IntegerField()
+    cost_price=models.FloatField()
+    purchase_date=models.DateTimeField(default=timezone.now())
+
+class Sales(models.Model):
+    invoice_number=models.CharField()
+    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    subtotal=models.DecimalField(max_digits=11, decimal_places=2)
+    gst_amount = models.DecimalField(max_digits=11, decimal_places=2)
+    total_amount=models.DecimalField(max_digits=11, decimal_places=2)
+    sale_date = models.DateTimeField(default=timezone.now())
+
+class SalesItems(models.Model):
+    medicine_ID = models.ForeignKey(Medicines, on_delete=models.CASCADE)
+    sale_ID = models.ForeignKey(Sales, on_delete=models.CASCADE)
+    quantity_sold = models.IntegerField()
+    unit_price=models.DecimalField(max_digits=11, decimal_places=2)
+    total_price = models.DecimalField(max_digits=11, decimal_places=2)
